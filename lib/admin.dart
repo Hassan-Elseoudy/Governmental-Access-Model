89d47e2dc11101e706a3eda2da262df02f9cd988
Merna177 <ronazakaria@gmail.com>
2019-10-06 12:56:51 +0200

    Admin Class

---
+++ lib/admin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class  Admin {
+  String id ;
+  Admin(String id) {
+    this.id = id;
+  }
+  get_request(){
+
+  }
+  write_to_confirmed(){
+
+  }
+  write_to_not_confirmed(){
+
+  }
+}
